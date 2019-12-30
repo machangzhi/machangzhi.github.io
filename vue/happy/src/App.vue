59218c237290fd59414e69755f3f50b58cb6e870
machangzhi <284551878@qq.com>
2019-12-30 19:49:36 +0800

    改为hash路由

--- vue/happy/src/App.vue
+++ vue/happy/src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div>
+      <button @click="goAbout">go {{url}}</button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private url: String = "about";
+  private goAbout(): void {
+    if (this.url === "about") {
+      this.$router.push(`/about`);
+      this.url = "home";
+    } else {
+      this.$router.push(`/`);
+      this.url = "about";
+    }
+  }
+}
 </script>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
